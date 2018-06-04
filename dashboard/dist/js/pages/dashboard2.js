66a751165819fa36d4c31b0e1e6bb19972a69116
MA Yuanwei <coldtiger@sjtu.edu.cn>
2018-06-04 19:18:09 +0800

    add index page

--- dashboard/dist/js/pages/dashboard2.js
+++ dashboard/dist/js/pages/dashboard2.js
@@ -2,6 +2,86 @@ $(function () {
 
   'use strict'
 
+  //---------------------------
+  //- END MONTHLY SALES CHART -
+  //---------------------------
+
+  //-------------
+  //- PIE CHART -
+  //-------------
+  // Get context with jQuery - using jQuery's .get() method.
+  var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+  var pieChart       = new Chart(pieChartCanvas)
+  var PieData        = [
+    {
+      value    : 700,
+      color    : '#dc3545',
+      highlight: '#dc3545',
+      label    : 'Chrome'
+    },
+    {
+      value    : 500,
+      color    : '#28a745',
+      highlight: '#28a745',
+      label    : 'IE'
+    },
+    {
+      value    : 400,
+      color    : '#ffc107',
+      highlight: '#ffc107',
+      label    : 'FireFox'
+    },
+    {
+      value    : 600,
+      color    : '#17a2b8',
+      highlight: '#17a2b8',
+      label    : 'Safari'
+    },
+    {
+      value    : 300,
+      color    : '#007bff',
+      highlight: '#007bff',
+      label    : 'Opera'
+    },
+    {
+      value    : 100,
+      color    : '#6c757d',
+      highlight: '#6c757d',
+      label    : 'Navigator'
+    }
+  ]
+  var pieOptions     = {
+    //Boolean - Whether we should show a stroke on each segment
+    segmentShowStroke    : true,
+    //String - The colour of each segment stroke
+    segmentStrokeColor   : '#fff',
+    //Number - The width of each segment stroke
+    segmentStrokeWidth   : 1,
+    //Number - The percentage of the chart that we cut out of the middle
+    percentageInnerCutout: 50, // This is 0 for Pie charts
+    //Number - Amount of animation steps
+    animationSteps       : 100,
+    //String - Animation easing effect
+    animationEasing      : 'easeOutBounce',
+    //Boolean - Whether we animate the rotation of the Doughnut
+    animateRotate        : true,
+    //Boolean - Whether we animate scaling the Doughnut from the centre
+    animateScale         : false,
+    //Boolean - whether to make the chart responsive to window resizing
+    responsive           : true,
+    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+    maintainAspectRatio  : false,
+    //String - A legend template
+    legendTemplate       : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>',
+    //String - A tooltip template
+    tooltipTemplate      : '<%=value %> <%=label%> users'
+  }
+  //Create pie or douhnut chart
+  // You can switch between pie and douhnut using the method below.
+  pieChart.Doughnut(PieData, pieOptions)
+  //-----------------
+  //- END PIE CHART -
+  //-----------------
   /* ChartJS
    * -------
    * Here we will create a few charts using ChartJS
@@ -84,86 +164,6 @@ $(function () {
   //Create the line chart
   salesChart.Line(salesChartData, salesChartOptions)
 
-  //---------------------------
-  //- END MONTHLY SALES CHART -
-  //---------------------------
-
-  //-------------
-  //- PIE CHART -
-  //-------------
-  // Get context with jQuery - using jQuery's .get() method.
-  var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
-  var pieChart       = new Chart(pieChartCanvas)
-  var PieData        = [
-    {
-      value    : 700,
-      color    : '#dc3545',
-      highlight: '#dc3545',
-      label    : 'Chrome'
-    },
-    {
-      value    : 500,
-      color    : '#28a745',
-      highlight: '#28a745',
-      label    : 'IE'
-    },
-    {
-      value    : 400,
-      color    : '#ffc107',
-      highlight: '#ffc107',
-      label    : 'FireFox'
-    },
-    {
-      value    : 600,
-      color    : '#17a2b8',
-      highlight: '#17a2b8',
-      label    : 'Safari'
-    },
-    {
-      value    : 300,
-      color    : '#007bff',
-      highlight: '#007bff',
-      label    : 'Opera'
-    },
-    {
-      value    : 100,
-      color    : '#6c757d',
-      highlight: '#6c757d',
-      label    : 'Navigator'
-    }
-  ]
-  var pieOptions     = {
-    //Boolean - Whether we should show a stroke on each segment
-    segmentShowStroke    : true,
-    //String - The colour of each segment stroke
-    segmentStrokeColor   : '#fff',
-    //Number - The width of each segment stroke
-    segmentStrokeWidth   : 1,
-    //Number - The percentage of the chart that we cut out of the middle
-    percentageInnerCutout: 50, // This is 0 for Pie charts
-    //Number - Amount of animation steps
-    animationSteps       : 100,
-    //String - Animation easing effect
-    animationEasing      : 'easeOutBounce',
-    //Boolean - Whether we animate the rotation of the Doughnut
-    animateRotate        : true,
-    //Boolean - Whether we animate scaling the Doughnut from the centre
-    animateScale         : false,
-    //Boolean - whether to make the chart responsive to window resizing
-    responsive           : true,
-    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-    maintainAspectRatio  : false,
-    //String - A legend template
-    legendTemplate       : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>',
-    //String - A tooltip template
-    tooltipTemplate      : '<%=value %> <%=label%> users'
-  }
-  //Create pie or douhnut chart
-  // You can switch between pie and douhnut using the method below.
-  pieChart.Doughnut(PieData, pieOptions)
-  //-----------------
-  //- END PIE CHART -
-  //-----------------
 
   /* jVector Maps
    * ------------
